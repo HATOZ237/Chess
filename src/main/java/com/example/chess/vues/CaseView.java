287d5aeb875cbcb17234ad7344ef8a5321b5b077
Johan Asangue <johanjoy2019@outlook.fr>
2022-01-08 10:05:49 -0500

    Johan à uploadé.

--- src/main/java/com/example/chess/vues/CaseView.java
+++ src/main/java/com/example/chess/vues/CaseView.java
@@ -1,26 +1,54 @@
 package com.example.chess.vues;
 
+import com.example.chess.modele.Echiquier;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-
 public class CaseView extends Rectangle {
-    private final String coordinates;
+    private final String Case;
+    private CaseViewState State;
 
-    public CaseView(String _coordinates, Color color) {
+    public CaseView(String p_case) {
         super();
-        coordinates = _coordinates;
+        Case = p_case;
+        State = CaseViewState.UNSELECTED;
 
-        setHeight(70);
-        setWidth(70);
-        setFill(color);
+        adjustCoordinates();
+        adjustColor();
+
+        System.out.println(getX());
     }
 
-    public String getCoordinates() {
-        return coordinates;
+    public Color getBaseColor() {
+        int x = Echiquier.axe_x.indexOf(String.valueOf(Case.charAt(0)));
+        int y = Echiquier.axe_y.indexOf(String.valueOf(Case.charAt(1)));
+
+        return (((x % 2) == 0 && (y % 2) == 0) || ((x % 2) != 0 && (y % 2) != 0))
+                ? Color.GRAY : Color.WHITE;
     }
 
-    public boolean matchCoordinates(String c) {
-        return coordinates == c;
+    public void adjustCoordinates() {
+        int x = Echiquier.axe_x.indexOf(String.valueOf(Case.charAt(0)));
+        int y = 7 - Echiquier.axe_y.indexOf(String.valueOf(Case.charAt(1)));
+
+        setX(x * 72); setY(y * 72);
+        setWidth(70); setHeight(70);
     }
+
+    public void adjustColor() {
+        switch (State) {
+            case UNSELECTED -> setFill(getBaseColor());
+            case SELECTED -> setFill(Color.RED);
+            case MOVE -> setFill(Color.ORANGE);
+        }
+    }
+
+    public void changeCaseState(CaseViewState p_state) {
+        if(p_state != State) {
+            State = p_state;
+            adjustColor();
+        }
+    }
+
+    public enum CaseViewState { UNSELECTED, SELECTED, MOVE; }
 }
--- src/main/java/com/example/chess/vues/PieceView.java
+++ src/main/java/com/example/chess/vues/PieceView.java
@@ -1,49 +1,36 @@
 package com.example.chess.vues;
 
-import com.example.chess.modele.Pion;
+import com.example.chess.modele.Couleur;
+import com.example.chess.modele.Echiquier;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Locale;
 
 public class PieceView extends ImageView {
-    private final Pion pion;
+    private final Couleur couleur;
+    private final String name;
 
-    public PieceView(Pion p) throws IOException {
-        setFitWidth(70);
-        setFitHeight(70);
+    public PieceView(String p_name, Couleur p_couleur, String pos) {
+        couleur = p_couleur;
+        name = p_name.toLowerCase();
 
-        pion = p;
-        System.out.println(pion);
-        String imageName = pion.getClass().getSimpleName().toLowerCase() + "_" + pion.getCouleur().toString().toLowerCase() + ".png";
-        Image img = new Image(getClass().getResource("/images/" + imageName).toString());
-        setImage(img);
-
-        adjustPosition();
+        adjustCoordinates(pos);
+        initImage();
     }
 
-    public void adjustPosition() throws IOException {
-        int[] pos = getCasesFromPosition(pion.getPosition());
+    public void adjustCoordinates(String pos) {
+        int x = Echiquier.axe_x.indexOf(String.valueOf(pos.charAt(0)));
+        int y = 7 - Echiquier.axe_y.indexOf(String.valueOf(pos.charAt(1)));
 
-        setX((pos[0] - 1) * 70);
-        setY((pos[1] - 1) * 70);
+        setFitWidth(70); setFitHeight(70);
+        setX(x * 72); setY(y * 72);
     }
 
-    public int[] getCasesFromPosition(String pos) throws IOException {
-        Character[] letters = new Character[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
-        Character[] numbers = new Character[] { '1', '2', '3', '4', '5', '6', '7', '8' };
-
-        if (pos.length() == 2 && Arrays.asList(letters).contains(pos.charAt(0))){
-            if (Arrays.asList(numbers).contains(pos.charAt(1))) {
-                int x = Arrays.asList(letters).indexOf(pos.charAt(0)) + 1;
-                int y = 9 - (Arrays.asList(numbers).indexOf(pos.charAt(1)) + 1);
-
-                return new int[] { x, y };
-            }
-        }
-
-        throw new IOException("The position format is not correct");
+    private void initImage() {
+        String url = getClass().getResource("/images/" +
+                name + "_" + couleur.toString().toLowerCase() + ".png").toString();
+        Image image = new Image(url);
+        setImage(image);
     }
 }
